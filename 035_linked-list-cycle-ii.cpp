6f77a5e11bbcbf9a0957ac4944568803258f84d0
Manish Kandpal <79416452+manishkandpal1@users.noreply.github.com>
2023-07-04 21:59:35 +0530

    Create 035_linked-list-cycle-ii.cpp

---
+++ 035_linked-list-cycle-ii.cpp
@@ -0,0 +1,52 @@
+//https://www.codingninjas.com/studio/problems/linked-list-cycle-ii_8230823?challengeSlug=striver-sde-challenge
+
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+            int data;
+            Node *next;
+            Node(){
+                this->data = 0;
+                next = NULL;
+            }
+            Node(int data){
+                this->data = data;
+                this->next = NULL;
+            }
+            Node(int data, Node* next) {
+                this->data = data;
+                this->next = next;
+            }
+        };
+
+
+*****************************************************************/
+
+Node *firstNode(Node *head)
+{
+     if(head==NULL || head->next==NULL){
+       return NULL;
+   }
+
+   Node * fast= head;
+   Node * slow= head;
+   Node * temp= head;
+
+   while(fast->next!=NULL && fast->next->next!=NULL){
+       slow= slow->next;
+       fast= fast->next->next;
+
+       if(fast==slow){ 
+       while(slow!=temp){
+           slow= slow->next;
+           temp= temp->next;
+       }
+        return temp;
+       }
+   }
+   return NULL;
+}
