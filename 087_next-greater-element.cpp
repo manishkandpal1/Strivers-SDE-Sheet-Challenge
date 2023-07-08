45f78286b6801fc01b411ec5add618137d562afa
Manish Kandpal <79416452+manishkandpal1@users.noreply.github.com>
2023-07-08 22:29:46 +0530

    Create 087_next-greater-element.cpp

---
+++ 087_next-greater-element.cpp
@@ -0,0 +1,35 @@
+//https://www.codingninjas.com/studio/problems/next-greater-element_8230718?challengeSlug=striver-sde-challenge
+
+
+#include <bits/stdc++.h> 
+
+vector<int> nextGreater(vector<int> &arr, int n) {
+    stack<int> st;
+    vector<int> ans;
+
+    for(int i=n-1; i>=0; i--){
+        if(st.empty()){
+            ans.push_back(-1);
+            st.push(arr[i]);
+        }
+        else if(arr[i]<st.top()){
+              ans.push_back(st.top());
+              st.push(arr[i]);
+        }
+        else{
+            while(!st.empty()){
+                if(arr[i]<st.top())
+                break;
+                st.pop();
+            }
+            if(st.empty())
+            ans.push_back(-1);
+            else
+            ans.push_back(st.top());
+            st.push(arr[i]);
+        }
+
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
