0402a4859432f675f01427dc24fefe718fac907d
Manish Kandpal <79416452+manishkandpal1@users.noreply.github.com>
2023-07-11 20:08:22 +0530

    Create 061_find-nth-root-of-m.cpp

---
+++ 061_find-nth-root-of-m.cpp
@@ -0,0 +1,18 @@
+//https://www.codingninjas.com/studio/problems/find-nth-root-of-m_8230799?challengeSlug=striver-sde-challenge
+
+int NthRoot(int n, int m) {
+  if(m == 1 || n == 1)
+      return m;
+    int start = 2, end = m - 1,mid = 0;
+    while(start <= end)
+    {
+        mid = start + (end - start)/2;
+        if(pow(mid, n) == m)
+          return mid;
+        else if(pow(mid, n) > m)
+          end = mid - 1;
+        else  
+          start = mid + 1;
+    }
+    return -1;
+}
