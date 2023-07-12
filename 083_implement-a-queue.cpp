248e1d6f2ca64d804b74b4d011791714c761f1b0
Manish Kandpal <79416452+manishkandpal1@users.noreply.github.com>
2023-07-12 23:22:07 +0530

    Create 083_implement-a-queue.cpp

---
+++ 083_implement-a-queue.cpp
@@ -0,0 +1,61 @@
+//https://www.codingninjas.com/studio/problems/implement-a-queue_8230848?challengeSlug=striver-sde-challenge
+
+#include <bits/stdc++.h> 
+
+class Queue {
+public:
+    int *arr;
+    int size;
+    int frnt;
+    int rear;
+
+    Queue() {
+        arr=new int[10000];
+        frnt=0;
+        rear=0;
+    }
+    /*----------------- Public Functions of Queue -----------------*/
+    bool isEmpty() {
+        if(frnt == rear){
+            return true;
+        }
+        return false;
+    }
+
+    void enqueue(int data) {
+        if(rear==size-1){
+            return;
+        }
+        else{
+            arr[rear]=data;
+            rear++;
+        }
+    }
+
+    int dequeue() {
+        int ans;
+        if(isEmpty()){
+            return -1;
+        }
+        else{
+            ans=arr[frnt];
+            frnt++;
+            if(frnt == rear){
+                frnt=0;
+                rear=0;
+            }
+        }
+        return ans;
+    }
+
+    int front() {
+        int ans;
+        if(isEmpty()){
+            return -1;
+        }
+        else{
+             ans=arr[frnt];
+        }
+        return ans;
+    }
+};
