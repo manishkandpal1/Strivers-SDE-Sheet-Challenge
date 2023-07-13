f75f2e305851667fb629f28f8faf72ccfa51b187
Manish Kandpal <79416452+manishkandpal1@users.noreply.github.com>
2023-07-13 18:18:25 +0530

    Create 093_maximum-in-sliding-windows-of-size-k.cpp

---
+++ 093_maximum-in-sliding-windows-of-size-k.cpp
@@ -0,0 +1,15 @@
+//https://www.codingninjas.com/studio/problems/maximum-in-sliding-windows-of-size-k_8230772?challengeSlug=striver-sde-challenge
+
+#include <bits/stdc++.h> 
+vector<int> slidingWindowMaximum(vector<int> &nums, int &k){
+    vector<int> ans;
+    deque<int> dq;
+    for(int i=0;i<nums.size();i++){
+        if(!dq.empty() && dq.front()==i-k) dq.pop_front();
+        while(!dq.empty() && nums[dq.back()]<=nums[i])
+        dq.pop_back();
+        dq.push_back(i);
+        if(i>=k-1) ans.push_back(nums[dq.front()]);
+    }
+    return ans;
+}
