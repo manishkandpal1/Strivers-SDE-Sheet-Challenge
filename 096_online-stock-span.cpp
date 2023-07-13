ee4ac1e56d5f2cd920b5f5e08d8e4cff12c045d5
Manish Kandpal <79416452+manishkandpal1@users.noreply.github.com>
2023-07-13 20:13:31 +0530

    Create 096_online-stock-span.cpp

---
+++ 096_online-stock-span.cpp
@@ -0,0 +1,19 @@
+//https://www.codingninjas.com/studio/problems/online-stock-span_8230843?challengeSlug=striver-sde-challenge
+
+#include <bits/stdc++.h> 
+
+vector<int> findSpans(vector<int> &price) {
+    stack<pair<int,int>> s;
+    vector<int> ans;
+    for(int i=0;i<price.size();i++){
+        int day=1;
+        while(!s.empty() && s.top().first<=price[i]){
+            day+=s.top().second;
+            s.pop();
+        }
+        s.push({price[i],day});
+        ans.push_back(day);
+    }
+    return ans;
+
+}
