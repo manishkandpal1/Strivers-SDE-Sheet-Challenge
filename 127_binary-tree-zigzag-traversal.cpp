5fa39ebb56bf9af26670e4c63d0c854d4ff6fa21
Manish Kandpal <79416452+manishkandpal1@users.noreply.github.com>
2023-07-16 16:21:12 +0530

    Create 127_binary-tree-zigzag-traversal.cpp

---
+++ 127_binary-tree-zigzag-traversal.cpp
@@ -0,0 +1,50 @@
+//https://www.codingninjas.com/studio/problems/binary-tree-zigzag-traversal_8230796?challengeSlug=striver-sde-challenge
+
+#include <bits/stdc++.h> 
+/*************************************************************
+
+    Following is the Binary Tree node structure
+
+    class BinaryTreeNode
+    {
+    public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+*************************************************************/
+
+vector<int> zigZagTraversal(BinaryTreeNode<int> *root) {
+    vector<int> ans;
+    if(root == NULL) return ans;
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    bool flag = true;
+    while(!q.empty()){
+        int n = q.size();
+        vector<int> temp(n);
+        for(int i = 0; i < n; i++){
+            BinaryTreeNode<int> *curr = q.front();
+            q.pop();
+            if(flag)
+                temp[i] = curr->data;
+            else
+                temp[n - i - 1] = curr->data;
+            if(curr->left != NULL)
+                q.push(curr->left);
+            if(curr->right != NULL)
+                q.push(curr->right);
+        }
+        for(auto it : temp){
+            ans.push_back(it);
+        }
+        flag = !flag;
+    }
+}
