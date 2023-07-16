859258267e32927cabd5a86abcd04db2f23297c3
Manish Kandpal <79416452+manishkandpal1@users.noreply.github.com>
2023-07-16 16:01:20 +0530

    Create 125_lca-of-binary-tree.cpp

---
+++ 125_lca-of-binary-tree.cpp
@@ -0,0 +1,41 @@
+//https://www.codingninjas.com/studio/problems/lca-of-binary-tree_8230760?challengeSlug=striver-sde-challenge
+
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+int lowestCommonAncestor(TreeNode<int> *root, int x, int y){
+    if(root == NULL){
+        return -1;
+    }
+    if(root->data == x || root->data ==y){
+        return root->data;
+    }
+
+    int left=lowestCommonAncestor(root->left,x,y);
+    int right=lowestCommonAncestor(root->right,x,y);
+
+    if(left == -1){
+        return right;
+    }
+    else if(right == -1){
+        return left;
+    }
+    return root->data;
+}
