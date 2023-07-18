3cab67aa806f02b877e8282016cb6d74282f8022
Manish Kandpal <79416452+manishkandpal1@users.noreply.github.com>
2023-07-18 22:02:16 +0530

    Create 161_detect-cycle-in-a-directed-graph.cpp

---
+++ 161_detect-cycle-in-a-directed-graph.cpp
@@ -0,0 +1,42 @@
+//https://www.codingninjas.com/studio/problems/detect-cycle-in-a-directed-graph_8230794?challengeSlug=striver-sde-challenge
+
+#include<bits/stdc++.h>
+
+int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
+    unordered_map<int, list<int>> adj;
+    for (int i = 0; i < edges.size(); i++) {
+        int u = edges[i].first-1;
+        int v = edges[i].second-1;
+        adj[u].push_back(v);
+    }
+
+    vector<int> indegree(n);
+    for (auto i : adj) {
+        for (auto j : i.second) {
+            indegree[j]++;
+        }
+    }
+
+    queue<int> q;
+    for (int i = 0; i < n; i++) {
+        if (indegree[i] == 0)
+            q.push(i);
+    }
+
+    int cnt = 0;
+    while (!q.empty()) {
+        int front = q.front();
+        q.pop();
+        cnt++;
+
+        for (auto neighbour : adj[front]) {
+            if (--indegree[neighbour] == 0)
+                q.push(neighbour);
+        }
+    }
+
+    if(cnt == n) return false;
+    else return true;
+
+}
+
