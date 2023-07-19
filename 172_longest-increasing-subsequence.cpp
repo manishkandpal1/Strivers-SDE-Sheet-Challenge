0a5c1bf370867dd77d2da012925022357cd497bf
Manish Kandpal <79416452+manishkandpal1@users.noreply.github.com>
2023-07-19 19:21:56 +0530

    Create 172_longest-increasing-subsequence.cpp

---
+++ 172_longest-increasing-subsequence.cpp
@@ -0,0 +1,19 @@
+//https://www.codingninjas.com/studio/problems/longest-increasing-subsequence_8230689?challengeSlug=striver-sde-challenge
+
+#include<bits/stdc++.h>
+using namespace std;
+int longestIncreasingSubsequence(int arr[], int n){
+      vector<int>dp;
+      int ans=1;
+      dp.push_back(arr[0]);
+      for (int i=1;i<n;i++) {
+          if (arr[i]>dp.back()){
+              dp.push_back(arr[i]);
+          }
+          else{
+              int ind=lower_bound(dp.begin(),dp.end(),arr[i])-dp.begin();
+              dp[ind]=arr[i];
+          }
+      }
+      return dp.size();
+}
